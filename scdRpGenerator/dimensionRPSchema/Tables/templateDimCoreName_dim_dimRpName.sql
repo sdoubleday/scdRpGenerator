4441e898c64dc636cd236d3ac7d4478eba6f95a9
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2021-06-10 08:36:50 -0400

    add output dimRp table. Note intentional absence of FK from the RPSK back to source. Such an FK confuses Excel since facts will also have FKs to various SKs. Also note that since we are not DOING Type6 generation here the DimensionalAttribute tag specifies the WHOLE column name including the _Cur suffix. Close #6

---
+++ scdRpGenerator/dimensionRPSchema/Tables/templateDimCoreName_dim_dimRpName.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dimensionRPSchema].[templateDimCoreName_dim_dimRpName]
+(
+	 [SK_templateDimCoreName] INT NOT NULL
+	,
+	 [SampleColumnOne_Cur] VARCHAR(500) NOT NULL		/*DimensionAttribute_ReplacementPoint|SampleColumnOne_Cur|,*/
+	,[SampleColumnTwo_Hist] VARCHAR(500) NOT NULL		/*Sample*/
+	,CONSTRAINT [pk_dimensionRPSchema_templateDimCoreName_dim_dimRpName] PRIMARY KEY CLUSTERED ([SK_templateDimCoreName])
+)
