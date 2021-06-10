2ca2081f3c5feb057310858f024de96b1dd00541
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2021-06-10 08:24:16 -0400

    add project for the dimRpSrc_vw which is the source we target to generate our RP objects. Close #2

---
+++ scdRpSourceView/Security/dimensionRpSrcSchema.sql
@@ -0,0 +1,1 @@
+﻿CREATE SCHEMA [dimensionRpSrcSchema] AUTHORIZATION [dbo];
---
+++ scdRpSourceView/dimensionRpSrcSchema/templateDimCoreName_RP_rolePlayName_dimRpSrc_vw.sql
@@ -0,0 +1,8 @@
+﻿CREATE VIEW [dimensionRpSrcSchema].[templateDimCoreName_RP_rolePlayName_dimRpSrc_vw]
+AS
+SELECT
+[SK_templateDimCoreName]
+,[d].[SampleColumnOne_Cur]
+,[d].[SampleColumnTwo_Hist]
+FROM [dimensionSchema].[templateDimCoreName_Dim] AS [d]
+;
