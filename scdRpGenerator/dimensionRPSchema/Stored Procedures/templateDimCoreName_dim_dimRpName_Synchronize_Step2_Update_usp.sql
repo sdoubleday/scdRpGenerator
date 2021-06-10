f294710891959d011c476352db07afe4fa847756
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2021-06-10 08:57:16 -0400

    add stubs for the synchronize sprocs

---
+++ scdRpGenerator/dimensionRPSchema/Stored Procedures/templateDimCoreName_dim_dimRpName_Synchronize_Step2_Update_usp.sql
@@ -0,0 +1,3 @@
+﻿CREATE PROCEDURE [dimensionRpSchema].[templateDimCoreName_dim_dimRpName_Synchronize_Step2_Update_usp]
+AS
+RETURN 0
---
+++ scdRpGenerator/dimensionRPSchema/Stored Procedures/templateDimCoreName_dim_dimRpName_Synchronize_usp.sql
@@ -0,0 +1,3 @@
+﻿CREATE PROCEDURE [dimensionRpSchema].[templateDimCoreName_dim_dimRpName_Synchronize_usp]
+AS
+RETURN 0
